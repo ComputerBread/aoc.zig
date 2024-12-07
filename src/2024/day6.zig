a1eaa10b75d5253b10a72923beefcfcd61ab88e8
ComputerBread <contact.computerbread@gmail.com>
2024-12-07 17:51:35 +0100

    day6 p2

--- src/2024/day6.zig
+++ src/2024/day6.zig
@@ -27,7 +27,7 @@ const Dir = enum {
 
     fn toBit(self: Dir) u8 {
         return switch (self) {
-            .UP => 1,
+            .UP => 0b1,
             .RIGHT => 0b10,
             .DOWN => 0b100,
             .LEFT => 0b1000,
@@ -46,7 +46,6 @@ pub fn part1(this: *const @This()) !?i64 {
             if (cell == '#') {
                 grid[i][j] = .blocked;
             } else if (cell == '^') {
-                std.debug.print("starting point at grid[{}][{}]\n", .{ i, j });
                 grid[i][j] = .visited;
                 r = i;
                 c = j;
@@ -55,7 +54,6 @@ pub fn part1(this: *const @This()) !?i64 {
     }
     var res: i64 = 1;
     while (moveForward(&r, &c, dir)) {
-        //std.debug.print("grid[{}][{}]\n", .{ r, c });
         if (grid[r][c] == .empty) {
             res += 1;
             grid[r][c] = .visited;
@@ -107,13 +105,16 @@ fn moveBackward(r: *usize, c: *usize, dir: Dir) void {
     }
 }
 
+// if you hit the same side of a block twice you should be in loop right?
 pub fn part2(this: *const @This()) !?i64 {
     var grid = [_][130]Type{[_]Type{.empty} ** 130} ** 130;
     var r: usize = 0;
     var c: usize = 0;
+
     var it = mem.tokenizeScalar(u8, this.input, '\n');
     var i: usize = 0;
     var dir = Dir.UP;
+
     while (it.next()) |row| : (i += 1) {
         for (row, 0..) |cell, j| {
             if (cell == '#') {
@@ -135,9 +136,8 @@ pub fn part2(this: *const @This()) !?i64 {
                 res += 1;
             }
             _ = moveForward(&r, &c, dir);
-            grid[r][c] = .empty;
+            grid[r][c] = .visited;
         } else if (grid[r][c] == .blocked) {
-            //grid[r][c] = .{ .bounced = dir.toBit() };
             moveBackward(&r, &c, dir);
             dir = dir.rotate();
         }
@@ -145,48 +145,49 @@ pub fn part2(this: *const @This()) !?i64 {
     return res;
 }
 
-fn isLooping(rr: usize, cc: usize, dir: Dir, grid: [130][130]Type) bool {
+fn isLooping(rr: usize, cc: usize, dir: Dir, g: [130][130]Type) bool {
     var r = rr;
     var c = cc;
     var d = dir;
-    var iter: u32 = 0;
+    var grid = g;
     while (moveForward(&r, &c, d)) {
-        if (iter > (130 * 131)) return true;
-        iter += 1;
-
-        //switch (grid[r][c]) {
-        // .bounced => |*val| {
-        //     if (val.* & d.toBit() != 0) {
-        //         return true;
-        //     }
-        //     val.* += d.toBit();
-        //     moveBackward(&r, &c, d);
-        //     d = d.rotate();
-        // },
-        // .blocked => {
-        //     grid[r][c] = .{ .bounced = d.toBit() };
-        //     moveBackward(&r, &c, d);
-        //     d = d.rotate();
-        // },
-        // else => {},
-        //}
-        if (grid[r][c] == .blocked) {
-            moveBackward(&r, &c, d);
-            d = d.rotate();
+        switch (grid[r][c]) {
+            .bounced => |*val| {
+                if (val.* & d.toBit() != 0) {
+                    return true;
+                }
+                val.* |= d.toBit();
+                moveBackward(&r, &c, d);
+                d = d.rotate();
+            },
+            .blocked => {
+                grid[r][c] = .{ .bounced = d.toBit() };
+                moveBackward(&r, &c, d);
+                d = d.rotate();
+            },
+            else => {},
         }
     }
     return false;
 }
 
-test "it should do nothing" {
-    const allocator = std.testing.allocator;
-    const input = "";
-
-    const problem: @This() = .{
-        .input = input,
-        .allocator = allocator,
-    };
-
-    try std.testing.expectEqual(null, try problem.part1());
-    try std.testing.expectEqual(null, try problem.part2());
+fn printGrid(g: [130][130]Type, rr: usize, cc: usize) void {
+    std.debug.print("bitch what? {} {}\n", .{ rr, cc });
+    for (0..10) |r| {
+        for (0..10) |c| {
+            if (r == rr and c == cc) {
+                std.debug.print("O", .{});
+                continue;
+            }
+            const p: u8 = switch (g[r][c]) {
+                .empty => '.',
+                .blocked => '#',
+                .visited => '^',
+                .bounced => '+',
+            };
+            std.debug.print("{c}", .{p});
+        }
+        std.debug.print("\n", .{});
+    }
+    std.debug.print("\n", .{});
 }
