b053dd379607d7212c4af6911c70cc5c2283732f
ComputerBread <contact.computerbread@gmail.com>
2024-12-11 10:14:13 +0100

    day 10

--- src/2024/day10.zig
+++ src/2024/day10.zig
@@ -11,18 +11,18 @@ const Dir = enum {
     LEFT,
 };
 
-const in =
-    \\89010123
-    \\78121874
-    \\87430965
-    \\96549874
-    \\45678903
-    \\32019012
-    \\01329801
-    \\10456732
-;
+// const in =
+//     \\89010123
+//     \\78121874
+//     \\87430965
+//     \\96549874
+//     \\45678903
+//     \\32019012
+//     \\01329801
+//     \\10456732
+// ;
 
-const SIZE = 8; // TODO: TO CHANGE
+const SIZE = 54; // TODO: TO CHANGE
 var grid = [_][SIZE]u8{[_]u8{0} ** SIZE} ** SIZE;
 
 fn moveForward(curr: @Vector(2, usize), dir: Dir) ?@Vector(2, usize) {
@@ -46,13 +46,14 @@ fn moveForward(curr: @Vector(2, usize), dir: Dir) ?@Vector(2, usize) {
         },
     }
     const sub = @subWithOverflow(grid[next[0]][next[1]], grid[curr[0]][curr[1]]);
-    std.debug.print("sub {}, overflow {}\n", .{ sub[0], sub[1] });
+    //std.debug.print("sub {}, overflow {}\n", .{ sub[0], sub[1] });
     if (sub[1] == 1 or sub[0] != 1) return null;
-    std.debug.print("({}, {}), ({}, {})\n", .{ next, grid[next[0]][next[1]], curr, grid[curr[0]][curr[1]] });
+    //std.debug.print("({}, {}), ({}, {})\n", .{ next, grid[next[0]][next[1]], curr, grid[curr[0]][curr[1]] });
     return next;
 }
 
 pub fn part1(this: *const @This()) !?i64 {
+    const in = this.input;
     var nodes = std.ArrayList(@Vector(2, usize)).init(this.allocator);
     defer nodes.deinit();
 
@@ -73,18 +74,78 @@ pub fn part1(this: *const @This()) !?i64 {
 
     var res: usize = 0;
     while (nodes.items.len != 0) {
-        const node = nodes.pop();
-        //std.debug.print("{} \n", .{node});
-        res += try append(&nodes, node, .UP);
-        res += try append(&nodes, node, .DOWN);
-        res += try append(&nodes, node, .LEFT);
-        res += try append(&nodes, node, .RIGHT);
+        res += try this.computeScore(nodes.pop());
     }
 
     return @intCast(res);
 }
 
-fn append(nodes: *std.ArrayList(@Vector(2, usize)), node: @Vector(2, usize), dir: Dir) !usize {
+fn append(nodes: *std.ArrayList(@Vector(2, usize)), node: @Vector(2, usize), dir: Dir, visited: *std.AutoHashMap(@Vector(2, usize), bool)) !usize {
+    const n = moveForward(node, dir);
+    if (n == null) {
+        return 0;
+    }
+    const v = n.?;
+    if (grid[v[0]][v[1]] == 9) {
+        if (visited.contains(v)) return 0;
+        try visited.put(v, true);
+        return 1;
+    }
+    try nodes.append(v);
+    return 0;
+}
+
+fn computeScore(this: *const @This(), node: @Vector(2, usize)) !usize {
+    var nodes = std.ArrayList(@Vector(2, usize)).init(this.allocator);
+    defer nodes.deinit();
+    try nodes.append(node);
+
+    var visited = std.AutoHashMap(@Vector(2, usize), bool).init(this.allocator);
+    defer visited.deinit();
+
+    var res: usize = 0;
+    while (nodes.items.len != 0) {
+        const n = nodes.pop();
+        res += try append(&nodes, n, .UP, &visited);
+        res += try append(&nodes, n, .DOWN, &visited);
+        res += try append(&nodes, n, .LEFT, &visited);
+        res += try append(&nodes, n, .RIGHT, &visited);
+    }
+    return res;
+}
+
+pub fn part2(this: *const @This()) !?i64 {
+    const in = this.input;
+    var nodes = std.ArrayList(@Vector(2, usize)).init(this.allocator);
+    defer nodes.deinit();
+
+    var lines = mem.tokenizeScalar(u8, in, '\n');
+    var r: usize = 0;
+    while (lines.next()) |line| : (r += 1) {
+        for (0..line.len) |c| {
+            if (line[c] < '0' or line[c] > '9') {
+                continue;
+            }
+            const val: u8 = line[c] - '0';
+            grid[r][c] = val;
+            if (val == 0) {
+                try nodes.append(.{ r, c });
+            }
+        }
+    }
+
+    var res: usize = 0;
+    while (nodes.items.len != 0) {
+        const n = nodes.pop();
+        res += try append2(&nodes, n, .UP);
+        res += try append2(&nodes, n, .DOWN);
+        res += try append2(&nodes, n, .LEFT);
+        res += try append2(&nodes, n, .RIGHT);
+    }
+
+    return @intCast(res);
+}
+fn append2(nodes: *std.ArrayList(@Vector(2, usize)), node: @Vector(2, usize), dir: Dir) !usize {
     const n = moveForward(node, dir);
     if (n == null) {
         return 0;
@@ -95,11 +156,6 @@ fn append(nodes: *std.ArrayList(@Vector(2, usize)), node: @Vector(2, usize), dir
     }
     try nodes.append(v);
     return 0;
-}
-
-pub fn part2(this: *const @This()) !?i64 {
-    _ = this;
-    return null;
 }
 
 test "it should do nothing" {
